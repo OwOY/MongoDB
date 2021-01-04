02b759fdba4c5bf6dfbba7204969da7cda3c669d
OwOY <63274030+OwOY@users.noreply.github.com>
2021-01-04 18:23:37 +0800

    Update mongo.py

--- mongo.py
+++ mongo.py
@@ -5,46 +5,52 @@ db = client.test
 collection = db.All
 
 # --------------------------輸入單筆資料-----------------------------
-# DB_dict = {
-#     'title':'yaya',
-#     'text':'yoyo',
-#     'datetime':'2019-01-06'
-# }
+'''
+DB_dict = {
+     'title':'yaya',
+     'text':'yoyo',
+     'datetime':'2019-01-06'
+ }
 
-# result = collection.insert_one(DB_dict)
+ result = collection.insert_one(DB_dict)
+'''
 # --------------------------輸入多筆資料-----------------------------
-# a = [1,2,3,4]
-# b = [2,4,5,6]
-# DB_dict = [
-#     {'title': c,'text': d,'test':'test'} for c,d in zip(a,b)
-# ]
-
-# result = collection.insert_many(DB_dict)
-# -------------------------------------------------------------------
-
+'''
+a = [1,2,3,4]
+b = [2,4,5,6]
+DB_dict = [
+   {'title': c,'text': d,'test':'test'} for c,d in zip(a,b)
+]
+result = collection.insert_many(DB_dict)
+'''
 
 # --------------------------尋找資料-----------------------------
-# finddata = collection.find({'BRAND':'APPLE'})
-# data = [data for data in finddata]
-# for d in data:
-#     print(d)
+'''
+finddata = collection.find({'BRAND':'APPLE'})
+data = [data for data in finddata]
+for d in data:
+    print(d)
+'''
+
 # ----------------------------尋找全部資料-------------------------
-# finddata = collection.find({})
-# data = [data for data in finddata]
-# print(data)
+'''
+finddata = collection.find({})
+data = [data for data in finddata]
+print(data)
+'''
+
 # --------------------------尋找Column-----------------------------
-# finddata = collection.find({}, {_id:0})    0為顯示   1為不顯示
-# data = [data for data in finddata]
-# for d in data:
-#     print(d)
-# -------------------------------------------------------------------
-
-
+'''
+finddata = collection.find({}, {_id:0})    0為顯示   1為不顯示
+data = [data for data in finddata]
+for d in data:
+    print(d)
+'''
 
 # ----------------------------刪除一筆資料-------------------------
-# mvone = collection.delete_one({'title':'yaya'})
+- mvone = collection.delete_one({'title':'yaya'})
 # ----------------------------刪除多筆資料-------------------------
-# mvmany = collection.delete_many({'title':'haha'})
+- mvmany = collection.delete_many({'title':'haha'})
 # ----------------------------刪除全部資料-------------------------
-# mvall = collection.delete_many({})
+- mvall = collection.delete_many({})
 # ----------------------------------------------------------------
